4a5c41851782da04466358dd050128b0642376fc
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-02-01 14:32:21 +0100

    Feat: read file content
    - read and display file contents on a page

--- wiki.rb
+++ wiki.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  nil
+end
+
 get('/') do
   erb :welcome
+end
+
+get '/:title' do
+  @title = params[:title]
+  @content = page_content @title
+  erb :show
 end
